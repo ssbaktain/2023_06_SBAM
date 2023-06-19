3123abba10ade2cbf78643b4d6845d6da96e7b48
ssbaktain <ssbaktain12@gmail.com>
2023-06-19 19:48:22 +0900

    db.txt 추가

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,51 +1,81 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.example.demo.service.ArticleService;
+import com.example.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
 	
-	private int lastArticleId;
-	private List<Article> articles;
+	private ArticleService articleService;
 	
-	public UsrArticleController() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
+	@Autowired
+	public UsrArticleController(ArticleService articleService) {
+		this.articleService = articleService;
 	}
 	
+	// 액션 메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		int id = this.lastArticleId + 1;
-		this.lastArticleId = id;
 		
-		Article article = new Article(id, title, body);
-		articles.add(article);
+		Article article = articleService.writeArticle(title, body);
 		
 		return article;
 	}
-	
+
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		return articles;
+		return articleService.getArticles();
 	}
-}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		return foundArticle;
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		articleService.modifyArticle(id, title, body);
+		
+		return id + "번 게시글을 수정했습니다";
+	}
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class Article {
-	private int id;
-	private String title;
-	private String body;
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		articleService.deleteArticle(id);
+		
+		return id + "번 게시글을 삭제했습니다";
+	}
 }
